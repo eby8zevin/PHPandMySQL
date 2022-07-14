f2d02c1f10c230db2d36fc289d16afdcef65346d
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-14 17:36:30 +0700

    Create User.php

---
+++ src/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+class User
+{
+    public function all()
+    {
+        echo "Ini adalah output dari model class User method all \n";
+        echo "<br>";
+    }
+}
